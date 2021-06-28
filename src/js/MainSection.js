d53db494f67b982779e3eaf060629f87088c63b8
Ramkrish26 <ramakrishnan2629@gmail.com>
2021-06-28 21:47:54 +0530

    Adding Four sections

--- src/js/MainSection.js
+++ src/js/MainSection.js
@@ -1,39 +1,53 @@
 import React, { Component } from 'react'
 import '../css/MainSection.css'
 import Home from './Home'
+import Register from './Register'
+import Login from './Login'
+import About from './About'
 
 class MainSection extends Component {
 
     constructor(props) {
         super(props)
-    
-        this.state = {
-             menuOption : 'Home'
-        }
-    }
-    
 
-    changeState = () => {
-        
+        this.state = {
+            menuOption: 'Home'
+        }
+
+        this.listRef = React.createRef()
     }
+
+    changeMenuOption = (event) => {
+        this.setState({
+            menuOption: event.target.textContent
+        })
+    }
+
+    displayMenuContent = () => {
+        if (this.state.menuOption === 'Home')
+            return <Home />
+        else if (this.state.menuOption === 'Register')
+            return <Register />
+        else if (this.state.menuOption === 'Login')
+            return <Login />
+        else if (this.state.menuOption === 'About')
+            return <About />
+    }
+
     render() {
+        let menuContent = this.displayMenuContent()
+
         return (
             <>
                 <div className='horizontalBar'>
-                    <div style={{ overflow: 'auto' }}>
-                        <div className='leftBar'>
-                            <ul>
-                                <li><button onClick={this.changeState}>Home</button></li>
-                                <li><button>Register</button></li>
-                                <li><button>Login</button></li>
-                                <li><button>About</button></li>
-                            </ul>
-                        </div>
-                    </div>                    
+                    <ul>
+                        <li><button onClick={this.changeMenuOption}>Home</button></li>
+                        <li><button onClick={this.changeMenuOption}>Register</button></li>
+                        <li><button onClick={this.changeMenuOption}>Login</button></li>
+                        <li><button onClick={this.changeMenuOption}>About</button></li>
+                    </ul>
                 </div>
-                <div>
-                    <Home></Home>
-                </div>
+                <div>{menuContent}</div>
             </>
         )
     }
